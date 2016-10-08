1248168bc0cf52023fbdc854b97fad4f65b05688
barambani <filippo.mariotti@gmail.com>
2016-10-08 23:41:50 +0100

    Added sbt build file. Added append to list

--- ListExample.scala
+++ ListExample.scala
@@ -1,46 +1,50 @@
 object ListExample {
 
-   object List {
+  object List {
+    def apply[A](as: A*): List[A] =
+      if(as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+  }
 
-      def apply[A](as: A*): List[A] =
-         if(as.isEmpty) Nil
-	 else Cons(as.head, apply(as.tail: _*))
+  sealed trait List[+A] extends Product with Serializable
+  final case object Nil extends List[Nothing]
+  final case class Cons[A](x: A, xs: List[A]) extends List[A]
 
-   }
+  def foldRightNotTailRec[A,B](as: List[A], z: B)(implicit f: (A,B) => B): B =
+    as match {
+      case Nil		=> z
+      case Cons(x, xs)	=> f(x, foldRightNotTailRec(xs, z))
+    }
 
-   sealed trait List[+A]
-   case object Nil extends List[Nothing]
-   case class Cons[A](x: A, xs: List[A]) extends List[A]
+  def foldRight[A,B]: (List[A], B) => ((B, A) => B) => B =
+    (xs, z) => f => foldLeft(reverse(xs), z)(f)
 
-
-   def foldRightBroken[A,B](as: List[A], z: B)(implicit f: (A,B) => B): B =
-      as match {
-         case Nil		=> z
-	 case Cons(x, xs)	=> f(x, foldRightBroken(xs, z))
-      }
-
-   def foldRight[A,B](xs: List[A], z: B)(f: (B, A) => B): B =
-      foldLeft(reverse(xs), z)(f)
-
-   def foldLeft[A,B](xs: List[A], z: B)(f: (B,A) => B): B = {
+  def foldLeft[A,B]: (List[A], B) => ((B,A) => B) => B = 
+    (xs, z) => f => {
       def iter(ys: List[A], acc: B): B = ys match {
-         case Nil		=> acc
-	 case Cons(w, ws)	=> iter(ws, f(acc, w)) 
+        case Nil	  => acc
+        case Cons(w, ws)  => iter(ws, f(acc, w)) 
       }
-
       iter(xs, z)
-   }
+    }
 
-   def sum(ns: List[Int]): Int =
-      foldRight(ns, 0)(_ + _)
+  def length[A]: List[A] => Int =
+    xs => foldRight(xs, 0) { (t, _) => t + 1 }
 
-   def prod(ns: List[Double]): Double =
-      foldRight(ns, 1d)(_ * _)
+  def reverse[A]: List[A] => List[A] =
+    xs => foldLeft(xs, Nil: List[A]) { (ys,y) => Cons(y, ys) }
 
-   def length[A](xs: List[A]): Int = 
-      foldRight(xs, 0){ (t, _) => t + 1 }
+  def append[A]: (List[A], A) => List[A] = appendLeft
 
-   def reverse[A](xs: List[A]): List[A] =
-      foldLeft(xs, Nil: List[A]){ (ys,y) => Cons(y, ys) }
+  def appendLeft[A]: (List[A], A) => List[A] =
+    (xs, x) => foldLeft(reverse(xs), Cons(x, Nil)) { (ys, y) => Cons(y, ys) }
+
+  def appendRight[A]: (List[A], A) => List[A] =
+    (xs, x) => foldRight(xs, Cons(x, Nil)) { (ys, y) => Cons(y, ys) }
+
+  lazy val sum: List[Int] => Int =
+    ns => foldRight(ns, 0)(_ + _)
+
+  lazy val prod: List[Double] => Double =
+    ns => foldRight(ns, 1d)(_ * _)
 }
-
---
+++ build.sbt
@@ -0,0 +1,18 @@
+lazy val commonSettings = Seq (
+  scalaVersion := "2.11.8"
+)
+
+lazy val simpleCqrsScala = (project in file(".")).settings(commonSettings: _*)
+
+libraryDependencies ++= Seq(
+  "org.scalaz" %% "scalaz-core" % "7.2.6"
+)
+
+initialCommands in console := """
+  |import java.util.UUID
+  |import scalaz._
+  |import Scalaz._
+  |import ListExample._
+""".stripMargin
+
+logLevel := Level.Info
