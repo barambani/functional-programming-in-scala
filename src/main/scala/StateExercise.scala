7d7605eff52e4886b7eebc6da32729fd021c3698
barambani <filippo.mariotti@gmail.com>
2017-08-15 01:46:28 +0100

    Completed up to Exercise 6.7

--- src/main/scala/StateExercise.scala
+++ src/main/scala/StateExercise.scala
@@ -52,4 +52,51 @@ object StateExercise {
         val (n, s1) = c._2.nextInt
         (n :: c._1, s1)
     }
+
+  type Rand[+A] = RNG => (A, RNG)
+  
+  val int: Rand[Int] = 
+    _.nextInt
+
+  def unit[A]: A => Rand[A] = 
+    a => rng => (a, rng)
+
+  def map[A,B]: Rand[A] => (A => B) => Rand[B] =
+    s => f => rng => {
+      lazy val (x, s1) = s(rng)
+      (f(x), s1)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt) { x => x - x % 2 }
+
+  def double2: Rand[Double] =
+    map(nonNegativeInt) { _.toDouble / Int.MaxValue.toDouble }
+
+  def map2[A,B,C]: Rand[A] => Rand[B] => ((A, B) => C) => Rand[C] =
+    ra => rb => f => rng => {
+      lazy val (a, s)  = ra(rng)
+      lazy val (b, s1) = rb(s)
+      (f(a, b), s1)
+    }
+
+  def both[A,B]: Rand[A] => Rand[B] => Rand[(A,B)] =
+    ra => rb => map2(ra)(rb){ (_, _) }
+
+  def intDouble2: Rand[(Int,Double)] =
+    both(int)(double2)
+
+  def doubleInt2: Rand[(Double,Int)] =
+    both(double2)(int)
+
+  def sequence[A]: List[Rand[A]] => Rand[List[A]] =
+    _.foldLeft(unit(Nil): Rand[List[A]]) { 
+      (xs, n) => map2(n)(xs) { _ :: _ }
+    }
+
+  def rngs[A]: Int => (=> Rand[A]) => Rand[List[A]] =
+    count => (sequence compose List.fill[Rand[A]](count))
+
+  def ints2: Int => Rand[List[Int]] =
+    count => rngs[Int](count)(int)
 }
